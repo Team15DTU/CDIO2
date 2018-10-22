69e6de302d7bbaa47aba5144407a8f1e696c3d5f
Karl Emil Jeppesen <32724999+KarlJeppesen@users.noreply.github.com>
2018-10-22 17:36:17 +0200

    main oprettet

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Karl Emil Jeppesen
+ * StudyID: s180557
+ * Date: 22-10-2018
+ * University: DTU
+ */
+public class Main {
+
+    public static void main (String[] args) {
+
+        //spil spillet
+
+    }
+
+}
