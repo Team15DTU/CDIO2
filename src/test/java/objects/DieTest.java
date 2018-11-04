70d374fd45a9a46025c392a09ac033a5a0faff36
Logstor <a.rottger_rydahl@live.dk>
2018-11-04 21:58:14 +0100

    Created DieTest.java

---
+++ src/test/java/objects/DieTest.java
@@ -0,0 +1,26 @@
+package objects;
+
+import static org.junit.Assert.*;
+
+public class DieTest {
+
+    @org.junit.Test
+    public void roll() {
+    }
+
+    @org.junit.Test
+    public void getFaceValue() {
+    }
+
+    @org.junit.Test
+    public void setFaceValue() {
+    }
+
+    @org.junit.Test
+    public void getSides() {
+    }
+
+    @org.junit.Test
+    public void setSides() {
+    }
+}
