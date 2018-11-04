7c77126823ad7a4d06da9d9145f509f37f7ad514
Logstor <a.rottger_rydahl@live.dk>
2018-11-04 13:41:17 +0100

    Some more documentation

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -21,6 +21,10 @@ public class Player {
     -------- Public Methods ----------
      */
 
+    /**
+     * Updates the players account with the specified amount
+     * @param score The amount which is going to the account as an int
+     */
     public void updateScore( int score ) {
         // Update the account associated with the player
         account.update(score);
