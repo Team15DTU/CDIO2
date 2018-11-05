d9d95d04f909ab1c1627ba669f2817fbee059bb4
Logstor <a.rottger_rydahl@live.dk>
2018-11-05 16:21:34 +0100

    Created DieTest.java with the initial test methods

---
+++ src/test/java/objects/CupTest.java
@@ -0,0 +1,34 @@
+package objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alfred Röttger Rydahl
+ * @date 05-11-2018
+ */
+public class CupTest {
+
+
+
+    @Test
+    public void cupRoll() {
+    }
+
+    @Test
+    public void getDies() {
+    }
+
+    @Test
+    public void setDies() {
+    }
+
+    @Test
+    public void getCupValue() {
+    }
+
+    @Test
+    public void setCupValue() {
+    }
+}
