991f1ba9f5b9ca3bcd6f2a28326500f938ebb647
Logstor <a.rottger_rydahl@live.dk>
2018-11-05 15:28:07 +0100

    Updated Fields and created 2 new constructors

--- src/main/java/objects/Cup.java
+++ src/main/java/objects/Cup.java
@@ -7,16 +7,36 @@ public class Cup {
      */
     private int cupValue;
 
-    private Die die1;
-    private Die die2;
+    private Die[] dies;
 
     /*
     ---------- Constructors -------
      */
-    public Cup () {
 
-        die1 = new Die();
-        die2 = new Die();
+    /**
+     * Default constructor which creates a cup with 2 dies of 6 sides.
+     */
+    public Cup () {
+        // Initialize
+        dies = new Die[2];
+
+        for (Die die : dies) {
+            die = new Die();
+        }
+    }
+
+    /**
+     * Alternative constructor, which creates a cup with "amountOfDies" dies of "dieSides" sides.
+     * @param amountOfDies int
+     * @param dieSides int
+     */
+    public Cup (int amountOfDies, int dieSides) {
+        // Initialize
+        dies = new Die[amountOfDies];
+
+        for (Die die : dies) {
+            die = new Die(dieSides);
+        }
     }
 
     /*
