65c94e3252373888ffa8f8cd1ccce74487aee88d
aksina <saaruppoulsen@gmail.com>
2018-11-05 16:51:34 +0100

    Update src/test/java/player/AccountTest.java
    update in the Account test

--- src/test/java/player/AccountTest.java
+++ src/test/java/player/AccountTest.java
@@ -17,10 +17,10 @@ public class AccountTest {
 
     @Test
     public void setBalance() {
-        Account setbal = new Account();
-        setbal.setBalance(1500);
+        Account getbal = new Account();
+        getbal.setBalance(1500);
 
-        assertEquals(1500,setbal.getBalance());
+        assertEquals(1500,getbal.getBalance());
     }
 
     @Test
