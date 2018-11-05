3135e884121fc3c0a8423df63cc3b744c6651355
Logstor <a.rottger_rydahl@live.dk>
2018-11-05 16:55:13 +0100

    Finished cupRoll() test

--- src/test/java/objects/CupTest.java
+++ src/test/java/objects/CupTest.java
@@ -18,6 +18,24 @@ public class CupTest {
 
     @Test
     public void cupRoll() {
+        /*
+        Roll the dies in the conventional cup and put the value into "first".
+        Check that the roll is in the correct range.
+        Check that the roll actually is different from time to time - THIS WILL EVENTUALLY FAIL
+         */
+        int first = cup.cupRoll();
+        assertTrue(first > 1 && first < 13);
+        assertTrue(first != cup.cupRoll());     // CAN FAIL
+
+        /*
+        Roll the dies in the pervert cup and put them into "first".
+        Check that the roll is in the correct range.
+        Check that the roll actually is different from time to time - THIS WILL EVENTUALLY FAIL
+         */
+        first = cup12.cupRoll();
+        assertTrue(first > 3 && first < 49);
+        assertTrue(first != cup12.cupRoll());   // CAN FAIL
+
     }
 
     @Test
