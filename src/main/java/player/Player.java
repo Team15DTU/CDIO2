3b7271ee0f979288dee11471afbd456511facd1b
Logstor <a.rottger_rydahl@live.dk>
2018-11-05 11:36:58 +0100

    Created a resetScore() in Player.java and a setter for account balance

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -45,9 +45,14 @@ public class Player {
      * Updates the players account with the specified amount
      * @param score The amount which is going to the account as an int
      */
-    public void updateScore( int score ) {
+    public void updateScore ( int score ) {
         // Update the account associated with the player
         account.update(score);
+    }
+
+    public void resetScore () {
+        // Set players account to 0
+        account.setBalance(0);
     }
 
     /*
@@ -59,7 +64,7 @@ public class Player {
      ------ Variable accessible ------
       */
 
-    public Account getAccount()  {return account; }
+    public Account getAccount()  { return account; }
 
     public void setAccount(Account account) { this.account = account; }
 
