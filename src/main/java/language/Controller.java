fa60f5bbe07a2a00fa68dfad6e915f4207aafe81
rasmussl <43174424+rasmussl@users.noreply.github.com>
2018-11-08 23:12:56 +0100

    Language - Controller: * Added description to methods * Added structure * Cleaned Getters and Setters

--- src/main/java/language/Controller.java
+++ src/main/java/language/Controller.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Controller {
 
+    /*
+    ----------- Fields -----------
+     */
+
     Language land = new Language();
 
     private static String fileName;
@@ -11,12 +15,25 @@ public class Controller {
 
     private static int input;
 
+    /*
+    ---------- Constructors -------
+     */
+
     public Controller () {
 
         fileName=Language.fileSwitch(input);
         language=Language.languageSwitch(input);
 
     }
+
+    /*
+    -------- Public Methods -------
+     */
+
+    /**
+     * This method asks the user about preferred language and sets it up.
+     * @return input from user which is used in setting up language.
+     */
 
     public static int selectorWindow() {
 
@@ -36,37 +53,24 @@ public class Controller {
         return input;
     }
 
-
-    // Variables Setters and Getters
+    /*
+    --------- Variables accessible ----
+     */
 
     public String getFileName(){return fileName;}
 
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
+    public void setFileName(String fileName) {this.fileName = fileName;}
 
-    public Language getLand() {
-        return land;
-    }
+    public Language getLand() {return land;}
 
-    public void setLand(Language land) {
-        this.land = land;
-    }
+    public void setLand(Language land) {this.land = land;}
 
-    public static int getInput() {
-        return input;
-    }
+    public static int getInput() {return input;}
 
-    public static void setInput(int input) {
-        Controller.input = input;
-    }
+    public static void setInput(int input) {Controller.input = input;}
 
-    public static String getLanguage() {
-        return language;
-    }
+    public static String getLanguage() {return language;}
 
-    public static void setLanguage(String language) {
-        Controller.language = language;
-    }
+    public static void setLanguage(String language) {Controller.language = language;}
 
 }
